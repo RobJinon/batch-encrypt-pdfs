8357f2234bfc9d7655b064cb90a9118089d25f5e
Jon Robien Jinon <robien.jinon@gmail.com>
2024-07-08 13:26:42 +0800

    Added folder dialogue box input

--- test.py
+++ test.py
@@ -2,11 +2,18 @@ from os import listdir
 from random import choice
 import pikepdf
 from pikepdf import Pdf
+import tkinter as tk
+from tkinter import filedialog
 
+root = tk.Tk()
+root.withdraw()
 
-Data=[]
-path="./"
+Data = []
+ 
+path = filedialog.askdirectory()
+
 OutputFolder="Output"
+
 pdfs=[ filename for filename in listdir(path) if filename.endswith(".pdf") ]
 
 def generate_password(filename):
@@ -15,19 +22,18 @@ def generate_password(filename):
     if strippedFilename.isdigit():
         return strippedFilename
     
+    # sets default password if filename is not numeric  
     return ''.join([choice('0123456789') for i in range(8)])
 
 
 for pdf in pdfs:
     password = generate_password(pdf)
 
-    print("Password: " + str(password))
-
-    with Pdf.open(f"{path}/{pdf}") as pdffile:
-        pdffile.save(f"{OutputFolder}/{pdf[:-4]}.pdf",encryption=pikepdf.Encryption(owner=password, user=password, R=4))
+    with Pdf.open(f"{path}/{pdf}", allow_overwriting_input=True) as pdffile:
+        pdffile.save(f"{path}/{pdf}",encryption = pikepdf.Encryption(owner=password, user=password, R=4))
 
     filename = pdf.replace('.pdf', '')
-
     Data.append(f"{filename},{password}")
 
 open("credentials.csv","a").writelines(s + '\n' for s in Data)
+print("Files encrypted successfully.")

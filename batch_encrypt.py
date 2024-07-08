b0e36cfe463833858575f143856c6775b2315530
Jon Robien Jinon <robien.jinon@gmail.com>
2024-07-08 15:13:37 +0800

    Added dialogue input for credentials file

--- batch_encrypt.py
+++ batch_encrypt.py
@@ -9,29 +9,22 @@ import pandas as pd
 root = tk.Tk()
 root.withdraw()
 
-Data = []
  
 path = filedialog.askdirectory()
+credentials = filedialog.askopenfile(filetypes = [('csv', '*.csv')], title="Select credentials file")
 
 OutputFolder="Output"
 
 pdfs=[ filename for filename in listdir(path) if filename.endswith(".pdf") ]
 
-# def generate_password(filename):
-#     strippedFilename = ''.join(i for i in filename if i.isdigit())
-
-#     if strippedFilename.isdigit():
-#         return strippedFilename
-    
-#     # sets default password if filename is not numeric  
-#     return ''.join([choice('0123456789') for i in range(8)])
 
 def read_password():
-    data = pd.read_csv("credentials.csv")
+    data = pd.read_csv(credentials)
     birthdays = data['Birthdate'].astype(str).tolist()
     return birthdays
 
 birthdays = read_password()
+
 
 # Changes the format of the date from DDMMYYYY to MMDDYYYY
 def format_date(date):
@@ -47,23 +40,16 @@ try:
     for pdf in pdfs:
         password = birthdays[counter]
 
+        filename = pdf.replace('.pdf', '')
+        
         with Pdf.open(f"{path}/{pdf}", allow_overwriting_input=True) as pdffile:
             pdffile.save(f"{path}/{pdf}",encryption = pikepdf.Encryption(owner=password, user=password, R=4))
 
-        filename = pdf.replace('.pdf', '')
-        Data.append(f"{filename},{password}")
-
-        # open("credentials.csv","a").writelines(s + '\n' for s in Data)
-
-        filename = pdf.replace('.pdf', '')
-        Data.append(f"{filename},{password}")
-        
         counter += 1
 
-# open("credentials.csv","a").writelines(s + '\n' for s in Data)
     tk.messagebox.showinfo(title = "Success", message = "Files encrypted successfully")
-    
+
 except Exception as error:
-    message = "Error (" + str(type(error).__name__) + "): Files already encrypted."
+    message = "Error: " + str(type(error).__name__)
 
     tk.messagebox.showwarning(title = "Error", message = message)
